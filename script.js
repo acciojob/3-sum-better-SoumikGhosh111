84944873182c33d83406d0f6ebaa7d4f3831e080
SoumikGhosh111 <132730107+SoumikGhosh111@users.noreply.github.com>
2023-08-15 21:10:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function threeSum(arr, target) {
 // write your code here
+	let arr2 = []; 
+	let start = 0; 
+	for(let i = 0; i<arr.length; i++){
+		for(let j = i+1, j<arr.length; j++){
+			for(let k = j+1; k<arr.length; k++){
+				arr2[start] = arr[i]+arr[j] + arr[k]; 
+				start++; 
+			}
+		}
+	}
+	console.log(arr2); 
   
 }
 
