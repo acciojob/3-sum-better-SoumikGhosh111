6aa5f12362d75d2e05d42d5eddc085717d52392c
SoumikGhosh111 <132730107+SoumikGhosh111@users.noreply.github.com>
2023-08-15 21:22:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,13 +5,32 @@ function threeSum(arr, target) {
 	for(let i = 0; i<arr.length; i++){
 		for(let j = i+1, j<arr.length; j++){
 			for(let k = j+1; k<arr.length; k++){
-				arr2[start] = arr[i]+arr[j] + arr[k]; 
+				arr2[start] = arr[i]+arr[j]+arr[k]; 
 				start++; 
 			}
 		}
 	}
-	console.log(arr2); 
-  
+	let arr3 = []; 
+	let start2 = 0; 
+	for(let i = 0; i<arr2.length; i++){
+		let max = Math.max(target, arr2[i]); 
+		let min = Math.min(target, arr2[i]); 
+		arr3[start2] = max - min; 
+		start2++; 
+	}
+	let minimum = Number.MAX_VALUE;  
+	for(let i = 0; i< arr3.length; i++){
+		if(arr3[i] < minimum){
+			minimum = arr3[i]; 
+		}
+	}
+	let indx = 0; 
+	for(let i = 0; i<arr3.length; i++){
+		if(minimum == arr3[i]){
+			indx = i; 
+		}
+	}
+	return arr2[indx]; 
 }
 
 module.exports = threeSum;
